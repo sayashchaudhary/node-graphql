24b008207b6033fa4e10e903fb36f5e4eb868530
sayashchaudhary <sayashchaudhary@gmail.com>
2020-09-12 23:00:57 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema")
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
+
+app.listen(3000, () => {
+    console.log('Server is running of port 3000');
+});
