03a286c184d3e86bdb2dff4993ab55f5d1e95352
sayashchaudhary <sayashchaudhary@gmail.com>
2020-09-13 00:16:40 +0530

    added schema for author

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -2,6 +2,7 @@ const graphql = require("graphql");
 const {
     GraphQLObjectType,
     GraphQLString,
+    GraphQLInt,
     GraphQLSchema,
     GraphQLID
 } = graphql;
@@ -14,12 +15,27 @@ let books = [
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
 ]
 
+let authors = [
+    {name: 'Patrick', age: 44, id: '1'},
+    {name: 'Jk Rowling', age: 43, id: '2'},
+    {name: 'William', age: 48, id: '3'}
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -33,6 +49,15 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 return _.find(books, { id: args.id })
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id })
             }
         }
     }
